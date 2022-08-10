ce638a063d4e95c395b468921491e550425a1e7a
Yaroslav <kfafamely@gmail.com>
2022-08-10 12:34:21 +0300

    add second code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 const numberOfFilms = +prompt('How match films are you see?', '');
 const personalMovieDB = {
     count: numberOfFilms,
@@ -13,4 +14,40 @@ const a = prompt('One of the last you films?', ''),
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+console.log(personalMovieDB);*/
+
+
+/*
+if (1){
+    console.log('ok');
+}else {
+    console.log('error')
+};
+
+
+if(num < 49){
+    console.log('error');
+}else if (num > 100){
+    console.log('so match');
+}else {
+    console.log('ok');
+}*/
+/*
+const num = 40;
+(num === 50) ? console.log('ok') : console.log('error');*/
+/*
+const num = '100';
+switch (num){
+    case '49':
+        console.log('error');
+        break;
+    case '100':
+        console.log('errror1');
+        break;
+    case '50':
+        console.log('ok');
+        break;
+    default:
+        console.log('good task')
+        break;
+}*/
